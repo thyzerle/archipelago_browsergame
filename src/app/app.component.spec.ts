41af07c327f0d69493cdacb6a577e565bd51a571
Thyzerle <timsegger+thyzerle@gmail.com>
2024-08-16 23:52:09 +0200

    chore: Another try

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'sample' title`, () => {
+  it(`should have the 'archipelago_browsergame' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sample');
+    expect(app.title).toEqual('archipelago_browsergame');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sample');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, archipelago_browsergame');
   });
 });
